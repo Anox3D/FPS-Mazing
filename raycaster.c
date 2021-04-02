fd4333cb2ba8ff628d1a67996d03cf0414ca45f3
Anox3D <71902913+Anox3D@users.noreply.github.com>
2021-04-02 19:58:15 +0200

    Maked code more readable

--- raycaster.c
+++ raycaster.c
@@ -34,7 +34,7 @@ int map[]=
 	1,0,0,0,0,0,0,1,
 	1,0,0,0,0,1,0,1,
 	1,0,0,0,0,0,0,1,
-	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1
 };
 
 void drawMap2D()
@@ -44,13 +44,18 @@ void drawMap2D()
 	{
 		for(x=0;x<mapX;x++)
 		{
-			if(map[y*mapX+x]==1){ glColor3f(1,1,1); } else{ glColor3f(0,0,0); }
+			if(map[y*mapX+x]==1){
+				glColor3f(1,1,1);
+			}
+			else {
+				glColor3f(0,0,0);
+			}
 			xo=x*mapS; yo=y*mapS;
 			glBegin(GL_QUADS);
-			glVertex2i(xo	  +1, yo	 +1);
-			glVertex2i(xo	  +1, yo+mapS-1);
+			glVertex2i(xo+1, yo+1);
+			glVertex2i(xo+1, yo+mapS-1);
 			glVertex2i(xo+mapS-1, yo+mapS-1);
-			glVertex2i(xo+mapS-1, yo	 +1);
+			glVertex2i(xo+mapS-1, yo+1);
 			glEnd();
 		}
 	}
@@ -58,52 +63,131 @@ void drawMap2D()
 
 float dist(float ax,float ay, float bx, float by, float ang0)
 {
-	return(sqrt((bx-ax)*(bx-ax) + (by-ay)*(by-ay)) );
+	return(sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay)));
 }
 
 void drawRays2D()
 {
 	int r,mx,my,mp,dof; float rx,ry,ra,xo,yo,disT;
-	ra=pa-DR*30; if(ra<0){ ra+=2*PI; } if(ra>2*PI){ ra-=2*PI; }
-	for(r=0;r<60;r++)
+	ra=pa-DR*30;
+	if(ra<0){
+		ra+=2*PI;
+	}
+	if(ra>2*PI){
+		ra-=2*PI;
+	}
+	for(r=0; r<60; 	r++)
 	{
-		// --- Check horizontal lines ---
+		// Check horizontal lines
 		dof=0;
-		float disH=1000000,hx=px,hy=py;
+		float disH=1000000, hx=px, hy=py;
 		float aTan=-1/tan(ra);
-		if(ra>PI){ ry=(((int)py>>6)<<6)-0.0001; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan; } // Looking up
-		if(ra<PI){ ry=(((int)py>>6)<<6); 		rx=(py-ry)*aTan+px; yo= 64; xo=-yo*aTan; } // Looking down	
-		if(ra==0 || ra==PI){ rx=px; ry=py; dof=8; } // Looking straight left or right
+		if(ra>PI){
+			ry=(((int)py>>6)<<6)-0.0001;	//
+			rx=(py-ry)*aTan+px; yo=-64;		// Looking up
+			xo=-yo*aTan;					//
+		}
+		if(ra<PI){
+			ry=(((int)py>>6)<<6);	//
+			rx=(py-ry)*aTan+px;		// Looking up
+			yo= 64; xo=-yo*aTan;	//
+		}
+		if(ra==0 || ra==PI){	//
+			rx=px; ry=py;		// Looking straight left or right
+			dof=8;				//
+		} 
 		while(dof<8)
 		{
 			mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;
-			if(mp>0 && mp<mapX*mapY && map[mp]>0){ hx=rx; hy=ry; disH=dist(px,py,hx,hy,ra); dof =8; } // Hit wall
-			else{ rx+=xo; ry+=yo; dof+=1; } // Next line
+			if(mp>0 && mp<mapX*mapY && map[mp]>0){
+				hx=rx; hy=ry;						//
+				disH=dist(px,py,hx,hy,ra);			// Hit wall
+				dof =8;								//
+			} 
+			else {
+				rx+=xo;	//
+				ry+=yo;	// Next line
+				dof+=1;	//
+			}
 		}
 		
-		// --- Check vertical lines ---
+		// Check vertical lines
 		dof=0;
-		float disV=1000000,vx=vx,vy=vy;
+		float disV=1000000, vx=vx, vy=vy;
 		float nTan=-tan(ra);
-		if(ra>P2 && ra<P3){ rx=(((int)px>>6)<<6)-0.0001; ry=(px-rx)*nTan+py; xo=-64; yo=-xo*nTan; } // Looking left
-		if(ra<P2 || ra>P3){ rx=(((int)px>>6)<<6); 		 ry=(px-rx)*nTan+py; xo= 64; yo=-xo*nTan; } // Looking right	
-		if(ra==0 || ra==PI){ rx=px; ry=py; dof=8; } // Looking straight up or down
+		if(ra>P2 && ra<P3){
+			rx=(((int)px>>6)<<6)-0.0001;	//
+			ry=(px-rx)*nTan+py; xo=-64;		// Looking left
+			yo=-xo*nTan;					//
+		}
+		if(ra<P2 || ra>P3){
+			rx=(((int)px>>6)<<6);		//
+			ry=(px-rx)*nTan+py; xo= 64;	// Looking right
+			yo=-xo*nTan;				//
+		}
+		if(ra==0 || ra==PI){
+			rx=px;				//
+			ry=py;				// Looking straight up or down
+			dof=8;				//
+		}
 		while(dof<8)
 		{
 			mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;
-			if(mp>0 && mp<mapX*mapY && map[mp]>0){ vx=rx; vy=ry; disV=dist(px,py,hx,hy,ra); dof =8; } // Hit wall
-			else{ rx+=xo; ry+=yo; dof+=1; } // Next line
+			if(mp>0 && mp<mapX*mapY && map[mp]>0){
+				vx=rx; vy=ry;						//
+				disV=dist(px,py,hx,hy,ra);			// Hit wall
+				dof =8;								//
+			}
+			else {
+				rx+=xo;	//
+				ry+=yo;	// Next line
+				dof+=1;	//
+			}
 		}
-		if(disV<disH){ rx=vx; ry=vy; disT=disV; glColor3f(0.9,0,0); } // Vertical wall hit
-		if(disH<disV){ rx=hx; ry=hy; disT=disH; glColor3f(0.7,0,0); } // Horizontal wall hit
-		glColor3f(1,0,0); glLineWidth(3); glBegin(GL_LINES); glVertex2i(px,py); glVertex2i(rx,ry); glEnd();
+		if(disV<disH){
+			rx=vx; ry=vy;		//
+			disT=disV;			// Vertical wall hit
+			glColor3f(0.9,0,0); //
+		}
+		if(disH<disV){
+			rx=hx; ry=hy;		//
+			disT=disH;			// Horizontal wall hit
+			glColor3f(0.7,0,0);	//
+		}
+		glColor3f(1,0,0);
+		glLineWidth(3);
+		glBegin(GL_LINES);
+		glVertex2i(px,py);
+		glVertex2i(rx,ry);
+		glEnd();
 		
-		// --- Draw 3D walls ---
-		float ca=pa-ra; if(ca<0){ ca+=2*PI; } if(ca>2*PI){ ca-=2*PI; } disT=disT*cos(ca); // Fix fisheye
-		float lineH=(mapS*320)/disT; if(lineH>320){ lineH=320; } // Line height
-		float lineO=160-lineH/2;								 // LIne offset
-		glLineWidth(8); glBegin(GL_LINES); glVertex2i(r*8+530,lineO); glVertex2i(r*8+530,lineH+lineO); glEnd();
-		ra+=DR; if(ra<0){ ra+=2*PI; } if(ra>2*PI){ ra-=2*PI; }
+		// Draw 3D walls
+		float ca=pa-ra;
+		if(ca<0){
+			ca+=2*PI;
+		}
+		if(ca>2*PI){
+			ca-=2*PI;
+		}
+		disT=disT*cos(ca); // Fix fisheye
+		float lineH=(mapS*320)/disT;
+		if(lineH>320){
+			lineH=320;	// Line height
+		}
+		float lineO=160-lineH/2;	// Line offset
+		glLineWidth(8);
+		glBegin(GL_LINES);
+		glVertex2i(r*8+530,lineO);
+		glVertex2i(r*8+530,lineH+lineO);
+		glEnd();
+		
+		ra+=DR;
+		if(ra<0){
+			ra+=2*PI;
+		}
+		if(ra>2*PI){
+			ra-=2*PI;
+		}
 	}
 }
 
@@ -118,10 +202,30 @@ void display()
 
 void buttons(unsigned char key, int x, int y)
 {
-	if(key=='a'){ pa-=0.1; if(pa<   0){ pa+=2*PI; } pdx=cos(pa)*5; pdy=sin(pa)*5; }
-	if(key=='d'){ pa+=0.1; if(pa>2*PI){ pa-=2*PI; } pdx=cos(pa)*5; pdy=sin(pa)*5; }
-	if(key=='w'){ px+=pdx; py+=pdy; }
-	if(key=='s'){ px-=pdx; py-=pdy; }
+	if(key=='a'){
+		pa-=0.1;
+		if(pa<0){
+			pa+=2*PI;
+		}
+		pdx=cos(pa)*5;
+		pdy=sin(pa)*5;
+	}
+	if(key=='d'){
+		pa+=0.1;
+		if(pa>2*PI){
+			pa-=2*PI;
+		}
+		pdx=cos(pa)*5;
+		pdy=sin(pa)*5;
+	}
+	if(key=='w'){
+		px+=pdx;
+		py+=pdy;
+	}
+	if(key=='s'){
+		px-=pdx;
+		py-=pdy;
+	}
 	glutPostRedisplay();
 }
 
